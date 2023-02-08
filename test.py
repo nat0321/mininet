242f6a97bd91fdb4fcbfd3b03e28294553dd26da
nat0321 <94549811+nat0321@users.noreply.github.com>
2023-02-08 16:46:57 -0600

    Add files via upload

--- test.py
+++ test.py
@@ -1,35 +1,38 @@
-from mininet.node import Controller
-from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.util import dumpNodeConnections
-from mininet.node import CPULimitedHost
-from mininet.link import TCLink
-from mininet.node import CPULimitedHost, Host, Node
-from mininet.node import IVSSwitch
-from mininet.node import OVSKernelSwitch, UserSwitch, Switch
-from mininet.node import OVSController, OVSSwitch, Controller
-from mininet.node import RemoteController
-from mininet.log import setLogLevel, info
+from mininet.node import Controller
 from mininet.cli import CLI
+from mininet.log import setLogLevel, info
 
-class Topology(Topo):
-    def __init__(self, bandwidth, **opts):
-        Topo.__init__(self, **opts)
- 
-        s1 = self.addSwitch('s1')
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
- 
-        self.addLink(h1, s1, bw=bandwidth)
-        self.addLink(h2, s1, bw=bandwidth)
- 
-def testIt():
-    topo = Topology(int(sys.argv[1]))
-    net = Mininet(topo, link=TCLink)
+def emptyNet():
+
+    "Create an empty network and add nodes to it."
+
+    net = Mininet( controller=Controller, waitConnected=True )
+
+    info( '*** Adding controller\n' )
+    net.addController( 'c0' )
+
+    info( '*** Adding hosts\n' )
+    h1 = net.addHost( 'h1', ip='10.0.0.1' )
+    h2 = net.addHost( 'h2', ip='10.0.0.2' )
+
+    info( '*** Adding switch\n' )
+    s3 = net.addSwitch( 's3' )
+
+    info( '*** Creating links\n' )
+    net.addLink( h1, s3 )
+    net.addLink( h2, s3 )
+
+    info( '*** Starting network\n')
     net.start()
-    h1, h2 = net.get('h1', 'h2')
-    net.iperf((h1,h2))
+
+    info( '*** Running CLI\n' )
+    CLI( net )
+
+    info( '*** Stopping network' )
     net.stop()
- 
+
+
 if __name__ == '__main__':
-    testIt()
+    setLogLevel( 'info' )
+    emptyNet()
